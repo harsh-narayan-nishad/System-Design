24f9ffa16d064b17335b5c8af33624fa3288efb2
Harsh_Narayan <harshnarayannishad@gmail.com>
2025-04-07 18:27:34 +0530

    First Commit

---
+++ LLD/LiskovExample.java
@@ -0,0 +1,33 @@
+package solid;
+
+public class LiskovExample {
+
+    // Parent class
+    public static class MenuItem {
+        public int price;
+        public String name;
+        public String description;
+
+        public MenuItem(int price, String name, String description) {
+            this.price = price;
+            this.name = name;
+            this.description = description;
+        }
+
+        public double getPrice() {
+            return this.price;
+        }
+    }
+
+    // Child class extending MenuItem
+    public static class BeverageItem extends MenuItem {
+
+        public BeverageItem(int price, String name, String description) {
+            super(price, name, description);
+        }
+
+        public double getPriceWithDiscount(int discountPercent) {
+            return this.price - (discountPercent * 0.01 * this.price);
+        }
+    }
+};
