d8057ee7ec76440505870211893ec9045716fcd5
Harsh_Narayan <harshnarayannishad@gmail.com>
2025-04-19 23:12:35 +0530

    LiskovExample and JavaPractice

--- LLD/LiskovExample1.java
+++ LLD/LiskovExample1.java
@@ -1,35 +1,34 @@
-public class LiskovExample {
+public class LiskovExample1 {
+    //parent class
 
-    // Parent class
     public static class MenuItem {
         public int price;
         public String name;
         public String description;
 
-        public MenuItem(int price, String name, String description) {
+        public MenuItem(int price, String name, String description){
             this.price = price;
             this.name = name;
             this.description = description;
         }
 
-        public double getPrice() {
+        public double getPrice(){
             return this.price;
         }
     }
 
-    // Child class extending MenuItem
-    public static class BeverageItem extends MenuItem {
+    // child class extanidn MenuItem,
 
-        public BeverageItem(int price, String name, String description) {
+    public static class BeverageItem extends MenuItem{
+        public BeverageItem(int price, String name, String description){
             super(price, name, description);
         }
 
-        public double getPriceWithDiscount(int discountPercent) {
+        public double  getPriceWithDiscount(int discountPercent){
             return this.price - (discountPercent * 0.01 * this.price);
         }
     }
 
-    // Main method
     public static void main(String[] args) {
         // Creating instances of MenuItem and BeverageItem
         MenuItem item = new MenuItem(100, "Burger", "A delicious beef burger");
