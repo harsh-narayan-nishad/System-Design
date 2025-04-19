5ef5e2a287030328befc958edd550c8f04fe3775
Harsh_Narayan <harshnarayannishad@gmail.com>
2025-04-19 19:49:35 +0530

    Java Basics

---
+++ Java/Pattern1.java
@@ -0,0 +1,11 @@
+public class Pattern1 {
+    public static void main(String[] args){
+        int row = 7;
+        for(int i = 1; i<row; i++){
+            for(int j = 1; j<i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
